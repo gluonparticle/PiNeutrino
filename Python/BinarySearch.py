340a9e6fdfa5342e5586e0a01b424aaed6040035
Sargun  Singh <138251601+gluonparticle@users.noreply.github.com>
2024-04-29 18:30:09 +0000

    Update BinarySearch.py

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -1,31 +1,27 @@
-def binary_search(arr, low, high, x):
-     # Check base case
-    if high >= low: 
-        mid = (high + low) // 2
- 
-       
-        if arr[mid] == x:
-            return mid
- 
+def binarySearch(array, x, low, high):
 
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
- 
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
+
+        mid = low + (high - low)//2
+
+        if array[mid] == x:
+            return mid
+
+        elif array[mid] < x:
+            low = mid + 1
 
         else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    
-    else:
-        # Element is not present in the array
-        return -1
+            high = mid - 1
+
+    return -1
  
 # Test array
 arr = [ 2, 3, 4, 10, 40 ]
 x = 10
  
 # Function call
-result = binary_search(arr, 0, len(arr)-1, x)
+result = binary_search(arr, x, 0,len(arr)-1)
  
 if result != -1:
     print("Element is present at index", str(result))
