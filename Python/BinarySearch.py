0623dccbe1b2df9c02b018038ad611339df3673c
Sargun  Singh <138251601+gluonparticle@users.noreply.github.com>
2024-04-29 18:16:09 +0000

    Create BinarySearch.py

---
+++ Python/BinarySearch.py
@@ -0,0 +1,36 @@
+n=int(input("enter the number of elements:"))
+array=[]
+
+for i in range(n):
+    x=float(input("enter value"))
+    array.append(x)
+
+array.sort()
+  
+def binarySearch(array, x, low, high):
+
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
+
+        mid = low + (high - low)//2
+
+        if array[mid] == x:
+            return mid
+
+        elif array[mid] < x:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
+
+
+
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
