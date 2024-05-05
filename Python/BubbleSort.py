3833936f4d9260aa7897ec0c525883b4e68355a0
Sargun  Singh <138251601+gluonparticle@users.noreply.github.com>
2024-05-05 18:37:04 +0000

    Update BubbleSort.py

--- Python/BubbleSort.py
+++ Python/BubbleSort.py
@@ -4,8 +4,9 @@ def bubble_sort(arr):
         for j in range(0, n-i-1): 
             if arr[j] > arr[j+1]:  
                 arr[j], arr[j+1] = arr[j+1], arr[j]  
+    return arr
 
 
-arr =[64, 34, 25, 12, 22, 11, 90]
+arr = [64, 34, 25, 12, 22, 11, 90]
 bubble_sort(arr)
 print ("Sorted array is:", arr)
