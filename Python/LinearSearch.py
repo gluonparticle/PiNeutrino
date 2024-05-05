3004f17aa311adea036258f02b8bddf9c9780b3b
Sargun  Singh <138251601+gluonparticle@users.noreply.github.com>
2024-05-05 18:29:26 +0000

    Update LinearSearch.py

--- Python/LinearSearch.py
+++ Python/LinearSearch.py
@@ -1,19 +1,23 @@
+def linearsearch(l,x):
+    n=len(arr)
+    for i in range(n):
+        if(l[i]==x):
+            return i
+    return -1
+    
+    
+    
+    
 n=int(input("enter the number of elements:"))
 l=[]
 
 for i in range(n):
     x=float(input("enter value"))
     l.append(x)
-
 print(l)
-
 x=float(input("enter the element to be found"))
 
-def linearsearch(l,n,x):
+
     
-    for i in range(n):
-        if(l[i]==x):
-            return i
-    return -1
-r=linearsearch(l,n,x)
+r=linearsearch(l,x)
 print(r)
