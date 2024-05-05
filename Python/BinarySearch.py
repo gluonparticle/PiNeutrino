f7b4f1625ad22224673e990f6e2736735c734246
Sargun  Singh <138251601+gluonparticle@users.noreply.github.com>
2024-05-05 18:01:22 +0000

    Update BinarySearch.py

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -3,7 +3,7 @@ def binarySearch(array, x, low, high):
     # Repeat until the pointers low and high meet each other
     while low <= high:
 
-        mid = low + (high - low)//2
+        mid = (high + low)//2
 
         if array[mid] == x:
             return mid
