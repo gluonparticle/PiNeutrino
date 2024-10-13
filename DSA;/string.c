df2547a0a1ba022f7145ccae75d742c77ce95e85
Gluon Particle <gluonparticle@gmail.com>
2024-10-13 15:57:36 +0000

    Changes to make it suitabler for the newesemestyer and account for lab exams

--- DSA;/string.c
+++ DSA;/string.c
@@ -1,44 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-char str[50],pat[50],rep[50],ans[50];
-int i,j,k,m,c,flag=0;
-void stringmatch(){
-    i=m=c=j=0;
-    while(str[c]!='\0'){
-        if(str[m]==pat[i]){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char str[50], pat[50], rep[50], ans[50];
+int i, j, k, m, c, flag = 0;
+
+void stringmatch() {
+    i = m = c = j = 0;
+    while (str[c] != '\0') {
+        if (str[m] == pat[i]) {
             i++;
             m++;
-            if(pat[i]=='\0'){
-                flag=1;
-                for(k=0;rep[k]!='\0';k++)
-                ans[j++]=rep[k];
-                c=m;
-                i=0;
+            if (pat[i] == '\0') {
+                flag = 1;
+                for (k = 0; rep[k] != '\0'; k++)
+                    ans[j++] = rep[k];
+                c = m;
+                i = 0;
             }
-        }
-        else{
-            ans[j]=str[c];
+        } else {
+            ans[j] = str[c];
             j++;
             c++;
-            m=c;
-            i=0;
+            m = c;
+            i = 0;
         }
     }
-int main(){
-    printf("\nEnter a Main String:\n");
-    gets(str);
-    printf("\nEnter a Patter String:\n");
-    gets(pat);
-    printf("\nEnter a Replacement String:\n");
-    gets(rep);
+    ans[j] = '\0'; // Null-terminate the resultant string
+}
+
+void main() {
+    printf("\nEnter a main string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Remove newline character
+
+    printf("\nEnter a Pattern String: ");
+    fgets(pat, sizeof(pat), stdin);
+    pat[strcspn(pat, "\n")] = '\0'; // Remove newline character
+
+    printf("\nEnter a Replacement String: ");
+    fgets(rep, sizeof(rep), stdin);
+    rep[strcspn(rep, "\n")] = '\0'; // Remove newline character
 
     stringmatch();
 
-    if(flag==1)
-    printf("\nThe resultant string is :\n%s",ans);
+    if (flag == 1)
+        printf("\nThe resultant string is: %s\n", ans);
     else
-    printf("\nPattern String not found!!\n");
-
-    return 0;
+        printf("\nPattern String not found!!\n");
 }
