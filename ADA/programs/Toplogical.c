b1ad4fc3a9892f861280e81873e5140cb67af179
Sargun  Singh <gluonparticle@gmail.com>
2025-04-08 23:25:42 +0000

    make piv as low ;

--- ADA/programs/Toplogical.c
+++ ADA/programs/Toplogical.c
@@ -15,28 +15,24 @@ void quicksort(int arr[], int low, int high) {
 }
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
+    int pivot = arr[low];
+    int i = low + 1;
     int j = high;
 
-    while (i < j){
-        
-        do {
+    while (i <= j) {
+        while (i <= high && arr[i] <= pivot) {
             i++;
-        } while (arr[i] <= pivot && i < high);
-
-        do {
+        }
+        while (arr[j] > pivot) {
             j--;
-        } while (arr[j] > pivot);
-
+        }
         if (i < j) {
             swap(&arr[i], &arr[j]);
         }
-
-    } 
-
-    swap(&arr[i], &arr[high]);
-    return i;
+    }
+    
+    swap(&arr[low], &arr[j]);
+    return j;
 }
 
 void swap(int* a, int* b) {
@@ -49,24 +45,19 @@ int main() {
     srand(time(NULL));
     int n = 100000;
     int elements[n];
-
     for (int i = 0; i < n; i++) {
         elements[i] = rand() % 1000;
     }
-
     for (int size = 5000; size <= n; size += 5000) {
         int arr[size];
         for (int i = 0; i < size; i++) {
             arr[i] = elements[i];
         }
-
         clock_t start = clock();
         quicksort(arr, 0, size - 1);
         clock_t end = clock();
-
         double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
         printf("Time taken to sort %d elements: %lf seconds\n", size, time_taken);
     }
-
     return 0;
 }
