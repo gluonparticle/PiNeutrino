3b89683227103d7fd9a5d75136c2b76a17e28827
Sargun  Singh <gluonparticle@gmail.com>
2025-05-22 22:30:25 +0000

    Add program 10;

---
+++ Java/program-10/ReadCookieServlet.java
@@ -0,0 +1,28 @@
+// File: ReadCookieServlet.java
+package javaprogram;
+
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class ReadCookieServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Cookie[] cookies = request.getCookies();
+        String empId = null;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("EMPID")) {
+                    empId = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        response.setContentType("text/html");
+        if (empId != null) {
+            response.getWriter().println("EMPID: " + empId);
+        } else {
+            response.getWriter().println("No cookie found with name EMPID.");
+        }
+    }
+}
