cc6e78e79e90515d9573cabc2f448bcca9bff37f
Sargun  Singh <gluonparticle@gmail.com>
2025-05-24 22:36:30 +0000

    Revert "Update SetCookieServlet.java"
    This reverts commit 0d46c1ac5103a0326ae0ebacf6579657f9ac7339.

--- Java/program-10/SetCookieServlet.java
+++ Java/program-10/SetCookieServlet.java
@@ -20,11 +20,12 @@ import java.io.IOException;
 public class SetCookieServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Cookie cookie = new Cookie("EMPID", "AN2356");
-        
         cookie.setMaxAge(3600);
         cookie.setPath("/");
+
         response.addCookie(cookie);
-        
+
+        response.setContentType("text/html");
         response.getWriter().println("Cookie 'EMPID' with value 'AN2356' is set.");
     }
 }
